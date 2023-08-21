37296b1aae8070da9b7fb521c6b552ca08024e23
Lemonjoy <1097000786@qq.com>
2023-08-21 18:32:46 +0800

    完成登录功能

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
+// 导入字体图标
+import './assets/font/iconfont.css'
+import axios from 'axios'
 
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/' // 设置请求的根路径
+Vue.prototype.$http = axios // 挂载原型的属性，发起http请求
 Vue.config.productionTip = false
 
 new Vue({
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Form, FormItem, Button, Input, Message } from 'element-ui'
+// 弹窗提示组件，需要全局挂载Message
+
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(Input)
+Vue.use(FormItem)
+Vue.prototype.$message = Message
